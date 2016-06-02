a0f77368c55b4cd51cbc7806fed06dd5305b9109
li <ant_li@qq.com>
2016-06-02 18:44:06 +0800

    添加测试代码

--- src/main/java/com/li/dubbo/consumer/Consumer.java
+++ src/main/java/com/li/dubbo/consumer/Consumer.java
@@ -1,28 +1,30 @@
 package com.li.dubbo.consumer;
 
 
-import java.util.List;
-
 import com.li.dubbo.provider.DemoService;
+import com.li.dubbo.provider.vo.User;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.List;
 
 
 public class Consumer {
 
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				new String[] { "applicationContext.xml" });
 		context.start();
 
-		DemoService<?> demoService = (DemoService<?>) context.getBean("demoService");
+		DemoService<User> demoService = (DemoService<User>) context.getBean("demoService");
 		String hello = demoService.sayHello("tom");
 		System.out.println(hello);
 
-		List<?> list = demoService.getUsers(10);
+		List<User> list = demoService.getUsers(10);
 		if (list != null && list.size() > 0) {
 			for (int i = 0; i < list.size(); i++) {
-				System.out.println(list.get(i));
+				System.out.println(list.get(i).getAge());
 			}
 		}
 		System.in.read();
